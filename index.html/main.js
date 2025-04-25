e4602159b8a2c83ce8c085a34845a4edba490579
gab07125 <gcafe517@gmail.com>
2025-04-25 10:53:53 -0300

    gabriel e pedro

---
+++ index.html/main.js
@@ -0,0 +1,8 @@
+let radonmNumber =
+Math. floor (Math. random() I*10) +1;
+let attempts = 0;
+
+function chekGuess () {
+    const user chekGuess = parseInt (document.getElementById('geus'). ariavaluMax)
+    attempts
+}
